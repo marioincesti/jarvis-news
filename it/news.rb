bf315ad556e360697b12121d382ed9a8705b2d2c
marioincesti <mario.incesti@vps30.com>
2017-02-19 23:01:54 +0100

    Update news.rb

--- it/news.rb
+++ it/news.rb
@@ -4,11 +4,11 @@ require 'rss'
 rss = RSS::Parser.parse('http://www.ansa.it/sito/ansait_rss.xml', false)
 arg=ARGV[0].to_s.downcase.capitalize
 rss.items.each do |item|
-       	if ARGV[0]=="oggi" 
+       	if ARGV[0]=="Oggi" 
         	puts "#{item.title}. #{item.description}\n"
 	elsif item.title+item.description =~ /.*#{arg}.*/
 		puts "#{$&}"
 	end
 end
-puts arg
+
 
