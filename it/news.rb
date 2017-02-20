eef73fc6c0aa0b23447d83c949cefaa4845cce7d
marioincesti <mario.incesti@vps30.com>
2017-02-20 01:54:42 +0100

    Update news.rb

--- it/news.rb
+++ it/news.rb
@@ -6,7 +6,7 @@ arg=ARGV[0].to_s.downcase.capitalize
 rss.items.each do |item|
        	if arg.length<1 
         	puts "#{item.title}.\t#{item.description}.\n"
-	elsif item.title+"\t"+item.description =~ /.*#{arg}.*/
+	elsif item.title+".\t"+item.description+".\t" =~ /.*#{arg}.*/
 		puts "#{$&}\n"
 	end
 end
